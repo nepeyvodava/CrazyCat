4ab8f96afd9152521fa9e67daa04a100a295275f
nepeyvodava <nepeyvodava@gmail.com>
2016-01-15 00:25:00 +0600

    bebebe

--- desktop/src/com/russianquest/game/desktop/DesktopLauncher.java
+++ desktop/src/com/russianquest/game/desktop/DesktopLauncher.java
@@ -6,7 +6,6 @@ import com.russianquest.game.RussianQuest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		String t = "xexe34";
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new RussianQuest(), config);
 	}
