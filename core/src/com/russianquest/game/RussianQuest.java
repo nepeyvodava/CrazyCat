3d393afc2f0fe92a5c417fbb6780ea80ee056251
nepeyvoda vladimir <nepeyvodava@gmail.com>
2016-01-25 16:14:11 +0600

    Cat Commit

--- core/src/com/russianquest/game/RussianQuest.java
+++ core/src/com/russianquest/game/RussianQuest.java
@@ -8,20 +8,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class RussianQuest extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture cat;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		cat = new Texture("FinalCat-64x128.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(cat, 0, 0);
 		batch.end();
 	}
 }
--- desktop/src/com/russianquest/game/desktop/DesktopLauncher.java
+++ desktop/src/com/russianquest/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.russianquest.game.RussianQuest;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Cat";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new RussianQuest(), config);
 	}
 }
