ab1acd17ebc8e1a1762a5448bfab7e0069d93515
nepeyvodava <nepeyvodava@gmail.com>
2016-01-26 22:56:10 +0600

    Cat Commit 2

--- core/src/com/russianquest/game/CrazyCat.java
+++ core/src/com/russianquest/game/CrazyCat.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class RussianQuest extends ApplicationAdapter {
+public class CrazyCat extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture cat;
 	
--- html/src/com/russianquest/game/client/HtmlLauncher.java
+++ html/src/com/russianquest/game/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.russianquest.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.russianquest.game.RussianQuest;
+import com.russianquest.game.CrazyCat;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new RussianQuest();
+                return new CrazyCat();
         }
 }
